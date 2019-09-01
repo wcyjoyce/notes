7782095844f42c0f137d9c0fe0cbeea1d3319403
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-09-02 07:10:10 +0800

    making sure that note title cannot be a duplicate

--- notes.js
+++ notes.js
@@ -3,11 +3,18 @@ const fs = require("fs");
 const addNote = (title, body) => {
   const notes = listNotes();
 
-  notes.push({
-    title: title,
-    body: body
-  });
-  saveNotes(notes);
+  // Checking to see if there are any duplicates
+  const duplicates = notes.filter(note => note.title === title);
+  if (duplicates.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    });
+    saveNotes(notes);
+    console.log("New note added!")
+  } else {
+    console.log("Note title cannot be a duplicate.")
+  };
 };
 
 const saveNotes = notes => {
