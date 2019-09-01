cf7c00b60f6f6841d7625f6497acad193ad52ff3
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-09-02 07:08:35 +0800

    returning empty array if JSON is empty

--- notes.js
+++ notes.js
@@ -2,11 +2,26 @@ const fs = require("fs");
 
 const addNote = (title, body) => {
   const notes = listNotes();
+
+  notes.push({
+    title: title,
+    body: body
+  });
+  saveNotes(notes);
+};
+
+const saveNotes = notes => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
 };
 
 const listNotes = () => {
-  const dataJSON = fs.readFileSync("notes.json").toString();
-  return JSON.parse(dataJSON);
+  try {
+    const dataJSON = fs.readFileSync("notes.json").toString();
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return [];
+  };
 };
 
 module.exports = {
